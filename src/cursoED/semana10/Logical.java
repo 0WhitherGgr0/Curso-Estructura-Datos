a13b356c436ab88d0ebd60f961f473d518c6bd10
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-05-27 15:24:13 -0500

    Modificaciones Semana 10 adicion de clases

---
+++ src/cursoED/semana10/Logical.java
@@ -0,0 +1,17 @@
+package cursoED.semana10;
+
+class Logical {
+	boolean value;
+
+	public Logical(boolean v) {
+		value = v;
+	}
+
+	public void setLogical(boolean v) {
+		value = v;
+	}
+
+	public boolean booleanValue() {
+		return value;
+	}
+}
